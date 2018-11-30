47ce58030cb1ff63c01d0429cfc21727c2bd0497
Griselda16 <45411288+Griselda16@users.noreply.github.com>
2018-11-29 18:17:48 -0600

    sistemas ordinario

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>proyecto sistemas :(</title>
+	<title>sistemas deinformacion Gris :(</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
