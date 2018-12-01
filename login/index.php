ea9fe0ea8ec97c629698d5a7bd96c6d736d90795
Griselda16 <45411288+Griselda16@users.noreply.github.com>
2018-12-01 15:28:03 -0600

    Griselda V.G 536

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>Sistemas gricelda:(</title>
+	<title>Vasquez Gonzalez Griselda sistemas de informacion:(</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
