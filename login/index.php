2efe14182252b6f046c94a1edbe5033780258ec7
Griselda16 <45411288+Griselda16@users.noreply.github.com>
2018-12-01 15:24:58 -0600

    modificaciones de subilme text

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>nombre del proyecto github:(</title>
+	<title>Sistemas gricelda:(</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
