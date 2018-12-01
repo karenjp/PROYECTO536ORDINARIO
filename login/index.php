ae82d6a4f947a8cef23a3eda6b7a81d8eeeb3968
Griselda16 <45411288+Griselda16@users.noreply.github.com>
2018-12-01 15:35:25 -0600

    sistemas modificacion del texto

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>Vasquez Gonzalez Griselda sistemas de informacion:(</title>
+	<title>griselda primer ordinario de sistemas:(</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
