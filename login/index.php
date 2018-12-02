746b10f1e9b6a5f31c80cafc029c4cfff1c25626
griselda16 <griceldavasquezgonzalez@gmail.com>
2018-12-01 19:09:19 -0600

    Proyecto sistemas

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>sublime tetx sistemas</title>
+	<title>Xammp reta de la carpeta</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
