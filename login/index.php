2cb5700a78777aedbe3a887ee17b9e2a1f07b564
griselda16 <griceldavasquezgonzalez@gmail.com>
2018-12-02 12:21:31 -0600

    practicas de github

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>ordinario2018</title>
+	<title>practicas de gris</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
