cfa0e6cf614aa774ab1fdb313bea37ee321f1eef
karen anahi <karenanahijp@gmail.com>
2018-12-03 12:25:42 -0600

    cambio titulo

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<titler>yamecansetituloxd</title>
+	<titler>titulo3</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
