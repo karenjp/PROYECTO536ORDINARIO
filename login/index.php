3c0f398372a215fe941470cc53aaa137e5951dbb
karen anahi <karenanahijp@gmail.com>
2018-12-03 12:10:19 -0600

    cambcontra

--- login/index.php
+++ login/index.php
@@ -35,7 +35,7 @@ if(isset($_SESSION['session']))
 					<p><label >elusuario:</label></p>
 						<input name="usuario" type="text" id="usuario" placeholder="Ingresa Usuario" autofocus="" required=""></p>
 					
-					<p><label>password1:</label></p>
+					<p><label>otracontraseña:</label></p>
 						<input name="contrasenia" type="password" id="contrasenia" placeholder="Ingresa Password" required=""></p>
 					
 					<p id="bot"><input type="submit" id="submit" name="submit" value="Ingresar" class="boton"></p>
