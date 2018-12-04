3b2876973384c2352b74914fd34f6b1265b5ffde
Aracely11 <45411122+Aracely11@users.noreply.github.com>
2018-12-04 09:41:23 -0600

    cambio github proyecto final

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<titler>sistemas de información Aracely</title>
+	<titler>github proyecto final</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
