27e11a7eda7a00cd00c934ff82db570bc2ccb51a
JonnyMttz <45440636+JonnyMttz@users.noreply.github.com>
2018-12-03 20:42:32 -0600

    cambiotitu563

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<titler>titulojonyp</title>
+	<titler>titulo563</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
