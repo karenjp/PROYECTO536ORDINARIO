90dd7e33fa5811569ebe5fb33245b1a6f37cd91a
griselda16 <griceldavasquezgonzalez@gmail.com>
2018-12-04 10:51:03 -0600

    ORDINARIO, GRIS

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<titler>GRISELDA 536, 5 SEMESTRE</title>
+	<titler>SISTEMAS DE INFORMACION, ACTIVIDAD DE ORDINARIO</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
