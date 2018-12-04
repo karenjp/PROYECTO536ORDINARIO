a45abb14b4e2e8c8066ddc2e2783a4765f6dbb8e
karen anahi <karenanahijp@gmail.com>
2018-12-03 23:17:21 -0600

    cambiodesierto

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<titler>eresmipedazo</title>
+	<titler>dedesierto</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
