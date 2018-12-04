1bf09e96903ddcc3ea5b801f541a22084c0af0ce
JonnyMttz <45440636+JonnyMttz@users.noreply.github.com>
2018-12-04 12:27:44 -0600

    cambiodetitulo

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<titler>cambiodia</title>
+	<titler>ultimodia</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
