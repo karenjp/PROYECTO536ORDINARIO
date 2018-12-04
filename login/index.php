ef225039393f0e04907a776532dae08916adcc02
Aracely11 <45411122+Aracely11@users.noreply.github.com>
2018-12-03 21:18:55 -0600

    cambio titulo11

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<titler>titulo563</title>
+	<titler>titulo11</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
