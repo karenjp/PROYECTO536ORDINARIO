34636f8601f5c03ff0d923068bdc45520bdc94ff
griselda16 <griceldavasquezgonzalez@gmail.com>
2018-12-04 10:45:08 -0600

    TITULO

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<titler>PROYECTO GITHUB</title>
+	<titler>PRACTICA DESDE SUBLIME TEXT</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
