55bb0c91be6ef855e8e2b0c47f55a0e0bd90cbb8
Aracely11 <45411122+Aracely11@users.noreply.github.com>
2018-12-04 10:57:24 -0600

    cambio ya casi

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<titler>proyecto github</title>
+	<titler>ya casi</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
@@ -32,7 +32,7 @@ if(isset($_SESSION['session']))
 					<!--A saber, el atributo for funciona como el id.-->
 					<!--ejemplo <label for="usuario">Usuario:</label>-->
 					<!--required es nuevo en html5, si el campo está vacío te avisa, pero cuidado, no valida la información-->
-					<p><label >usuariox3:</label></p>
+					<p><label >otroususario:</label></p>
 						<input name="usuario" type="text" id="usuario" placeholder="Ingresa Usuario" autofocus="" required=""></p>
 					
 					<p><label>contraseña3:</label></p>
